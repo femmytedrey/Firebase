c2763102a904565b2f01b7154fd9698498b72f79
femmytedrey <femmytedrey@gmail.com>
2024-01-24 20:46:09 +0100

    creating an authentication to sign in with google

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,8 +1,31 @@
+import { useState } from 'react'
+import { auth, googleProvider } from '../config/firebase'
+import { createUserWithEmailAndPassword, signInWithPopup } from 'firebase/auth'
 export const Auth = () => {
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const signIn = async () => {
+        try {
+            await createUserWithEmailAndPassword(auth, email, password)
+            setEmail('')
+            setPassword('')
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    const signinWithGoogle = async () => {
+        try{
+            await signInWithPopup(auth, googleProvider)
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return(
         <div>
-            <input placeholder="Email..." />
-            <input placeholder="Password..." />
+            <input type='text' placeholder="Email..." value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <input type='password' placeholder="Password..." value={password} onChange={(e) => setPassword(e.target.value)}/>
+            <button onClick={signIn}>Sign In</button>
+            <button onClick={signinWithGoogle}>Signin With Google</button>
         </div>
     )
 }
